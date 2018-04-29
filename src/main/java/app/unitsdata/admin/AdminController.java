0d73a254a689aa84fb2aa670f574badfb4c67d7c
igogo978 <chia0210@gmail.com>
2018-04-29 21:01:23 +0800

    admin home

--- src/main/java/app/unitsdata/admin/AdminController.java
+++ src/main/java/app/unitsdata/admin/AdminController.java
@@ -1,15 +1,15 @@
 package app.unitsdata.admin;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @RequestMapping("/home")
     public String adminHome() {
-        return "admin home";
+        return "admin/home";
     }
 }
